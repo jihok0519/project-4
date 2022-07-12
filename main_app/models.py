c3fa5a1c968c6e876efb12c9002e044dd44f6a63
Enoch Kim <jihok0519@gmail.com>
2022-07-12 15:56:12 -0400

    wip: working on signup/login

--- main_app/models.py
+++ main_app/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
 class Gift(models.Model):
     giftName=models.CharField(max_length=30)
     price=models.IntegerField(default=0)
-    
-        
+
     def __str__(self):
         return self.giftName
+    
+    def get_absolute_url(self):
+        return reverse('gifts_detail', kwargs={'pk': self.id})
 
 class Birthday(models.Model):
     name=models.CharField(max_length=50)
@@ -15,6 +19,7 @@ class Birthday(models.Model):
     relationship=models.CharField(max_length=30)
     venue=models.CharField(max_length=100)
     gift=models.ManyToManyField(Gift)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
