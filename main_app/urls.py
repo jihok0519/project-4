19683d5c0d8160f8a07ecefd30b684600acf2db3
Enoch Kim <jihok0519@gmail.com>
2022-07-12 21:44:08 -0400

    added gift form/delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path('gifts/create/', views.GiftCreate.as_view(), name='gifts_create'),
     path('gifts/<int:pk>/update/', views.GiftUpdate.as_view(), name='gifts_update'),
     path('gifts/<int:pk>/delete/', views.GiftDelete.as_view(), name='gifts_delete'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
